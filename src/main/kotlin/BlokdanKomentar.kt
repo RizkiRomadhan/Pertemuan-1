ae0bb85e9ac971f432df1eb1bb4b10103821bed3
ACER <rizkiromadhanbks298@gmail.com>
2022-03-10 15:00:17 +0700

    Ini Kode program yang digunakan untuk membuat blok dan komentar

---
+++ src/main/kotlin/BlokdanKomentar.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    println("Hello, ini program pertama saya di kotlin")
+    //Contoh komentar satu baris
+    /*
+    Contoh Komentar multi baris
+     */
+    /**
+     * Kotlin Doc --> baris ini untuk mendokumentasikan kode program
+     */
+}
